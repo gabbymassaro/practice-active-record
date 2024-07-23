127399ac3d5006c892755ae76f7c4e3cc64de2b0
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-23 15:10:10 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,17 @@ Conference.destroy_all
 Speaker.destroy_all
 
 puts "🌱 Creating conferences..."
-Company.create(name: "AI DevSummit", year: 2024)
-Company.create(name: "Data Science Festival", year: 2019)
-Company.create(name: "Dublin Tech Summit", year: 2005)
-Company.create(name: "Ruby Con", year: 2017)
+Conference.create(name: "AI DevSummit", year: 2024)
+Conference.create(name: "Data Science Festival", year: 2019)
+Conference.create(name: "Dublin Tech Summit", year: 2005)
+Conference.create(name: "Ruby Con", year: 2017)
 
 
 puts "🌱 Creating speakers..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+Speaker.create(name: "Rick")
+Speaker.create(name: "Morty")
+Speaker.create(name: "Mr. Meseeks")
+Speaker.create(name: "Gazorpazop")
 
 puts "🌱 Creating sessions..."
-# Create freebies Here
+# Create sessions Here
